3b9d9972ebe81d83449ba41b5a0e5af92b631741
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2024-10-30 12:49:22 -0300

    video 7 terminado

---
+++ src/persistence/localstorage.js
@@ -0,0 +1,46 @@
+//local storage
+
+export const handleGetProductsLocalStorage = ()=>{
+
+    const products= JSON.parse(localStorage.getItem("products"));
+    if(products){
+        return products;
+    } else{
+        return [];
+    }
+
+};
+
+//guardar en local storage
+
+//recibe un producto
+export const setInLocalStorage= (productIn)=>{
+    //trae los elementos
+    let producsInLocal=handleGetProductsLocalStorage();
+    
+    const existingIndex= producsInLocal.findIndex((productsLocal) =>
+        productsLocal.id==productIn.id
+    )
+
+    //verifica si el elemento existe
+    if(existingIndex!==-1){
+        //si existe debe reemplazarse
+        producsInLocal[existingIndex]=productIn;
+    } else {
+        //si no agregarse
+        producsInLocal.push(productIn);
+    }
+
+    localStorage.setItem("products",json.parse(producsInLocal));
+};
+
+
+
+
+
+
+
+
+
+
+
